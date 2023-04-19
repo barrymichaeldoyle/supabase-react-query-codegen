a3bba14a47ee09d97e33656b9f09d843af6e8543
Barry Michael Doyle <barry.doyle@universal.one>
2023-04-19 11:33:15 +0200

    updated eol settings to enforce consistency across dev OS

--- src/utils/spec/toHookName.spec.ts
+++ src/utils/spec/toHookName.spec.ts
@@ -1,7 +1,7 @@
 import { toHookName } from '../toHookName';
 
 describe('toHookName', () => {
-  it('should return the hook name for a table with a single word name and a non-GetAll operation', () => {
+  it('should return the hook name for a table with a single word name.', () => {
     const hookName = toHookName('users', 'Get');
     expect(hookName).toBe('useGetUser');
   });
@@ -29,5 +29,15 @@ describe('toHookName', () => {
   it('should return the hook name for a table with a multi-word snake_case name and the GetAll operation', () => {
     const hookName = toHookName('order_item_details', 'GetAll');
     expect(hookName).toBe('useGetAllOrderItemDetails');
+  });
+
+  it('should return the hook name for a table named people and a non-GetAll operation', () => {
+    const hookName = toHookName('people', 'Delete');
+    expect(hookName).toBe('useDeletePerson');
+  });
+
+  it('should return the hook name for a table named people and the GetAll operation', () => {
+    const hookName = toHookName('people', 'GetAll');
+    expect(hookName).toBe('useGetAllPeople');
   });
 });
